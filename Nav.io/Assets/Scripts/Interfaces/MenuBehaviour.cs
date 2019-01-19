ee38167efc78bb7a7f3d92a45c986bc5966199aa
Unknown <luizfilipemonge@outlook.com>
2019-01-19 06:16:59 -0200

    Main menu

---
+++ Nav.io/Assets/Scripts/Interfaces/MenuBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class MenuBehaviour : MonoBehaviour {
+
+	Animator cAnimator;
+
+
+	// Use this for initialization
+	void Start () {
+		cAnimator = gameObject.GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetButton("Fire1") && !cAnimator.GetBool("GameStart"))
+        {
+            cAnimator.SetBool("GameStart", true);
+        }
+	}
+
+	public void ChangeScene(string scene){
+		SceneManager.LoadScene(scene);
+	}
+}
---
+++ Nav.io/Assets/Scripts/Interfaces/SoundBehaviour.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundBehaviour : MonoBehaviour
+{
+
+    public GameObject source;
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    IEnumerator PlayClip(AudioClip clip)
+    {
+        AudioSource audio = source.GetComponent<AudioSource>();
+        audio.clip = clip;
+        audio.Play();
+        yield return new WaitForSeconds(audio.clip.length);
+    }
+}
